baf87c508322ba36e9e0883d888c8587a8de3949
Spyros Stathopoulos <spyros@arc-instruments.co.uk>
2022-03-16 16:48:40 +0000

    Initial infrastructure commit

---
+++ driver_init.c
@@ -0,0 +1,157 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+#include <hpl_gclk_base.h>
+#include <hpl_pm_base.h>
+
+struct usart_sync_descriptor USART_0;
+
+struct dac_sync_descriptor DAC_0;
+
+void SPI_0_PORT_init(void)
+{
+
+	gpio_set_pin_level(PA04,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PA04, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(PA04, PINMUX_PA04D_SERCOM0_PAD0);
+
+	gpio_set_pin_level(PA05,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PA05, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(PA05, PINMUX_PA05D_SERCOM0_PAD1);
+
+	// Set pin direction to input
+	gpio_set_pin_direction(PA06, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_pull_mode(PA06,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(PA06, PINMUX_PA06D_SERCOM0_PAD2);
+}
+
+void SPI_0_CLOCK_init(void)
+{
+	_pm_enable_bus_clock(PM_BUS_APBC, SERCOM0);
+	_gclk_enable_channel(SERCOM0_GCLK_ID_CORE, CONF_GCLK_SERCOM0_CORE_SRC);
+}
+
+void USART_0_PORT_init(void)
+{
+
+	gpio_set_pin_function(PA22, PINMUX_PA22C_SERCOM1_PAD0);
+
+	gpio_set_pin_function(PA23, PINMUX_PA23C_SERCOM1_PAD1);
+}
+
+void USART_0_CLOCK_init(void)
+{
+	_pm_enable_bus_clock(PM_BUS_APBC, SERCOM1);
+	_gclk_enable_channel(SERCOM1_GCLK_ID_CORE, CONF_GCLK_SERCOM1_CORE_SRC);
+}
+
+void USART_0_init(void)
+{
+	USART_0_CLOCK_init();
+	usart_sync_init(&USART_0, SERCOM1, (void *)NULL);
+	USART_0_PORT_init();
+}
+
+void delay_driver_init(void)
+{
+	delay_init(SysTick);
+}
+
+void DAC_0_PORT_init(void)
+{
+
+	// Disable digital pin circuitry
+	gpio_set_pin_direction(PA02, GPIO_DIRECTION_OFF);
+
+	gpio_set_pin_function(PA02, PINMUX_PA02B_DAC_VOUT);
+}
+
+void DAC_0_CLOCK_init(void)
+{
+
+	_pm_enable_bus_clock(PM_BUS_APBC, DAC);
+	_gclk_enable_channel(DAC_GCLK_ID, CONF_GCLK_DAC_SRC);
+}
+
+void DAC_0_init(void)
+{
+	DAC_0_CLOCK_init();
+	dac_sync_init(&DAC_0, DAC);
+	DAC_0_PORT_init();
+}
+
+void system_init(void)
+{
+	init_mcu();
+
+	// GPIO on PA03
+
+	gpio_set_pin_level(SWRST,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(SWRST, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(SWRST, GPIO_PIN_FUNCTION_OFF);
+
+	// GPIO on PA07
+
+	gpio_set_pin_level(CS,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(CS, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(CS, GPIO_PIN_FUNCTION_OFF);
+
+	SPI_0_CLOCK_init();
+	SPI_0_init();
+	SPI_0_PORT_init();
+
+	USART_0_init();
+
+	delay_driver_init();
+
+	DAC_0_init();
+}
